1de4210c847a65200b598717ed5530d7adc38184
Suguda-Thakur-Marndi <sugudathakurmarndi1106@gmail.com>
2026-03-12 19:36:57 +0530

    2nd commit

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,9 +1,9 @@
-const express = require('express');
+import express from 'express';
 const router = express.Router();
 
 router.get('/home', (req, res) => {
     res.render('home');
 });
 
-module.exports = router;
+export default router;
 
